7debf96639189989ed334a75f21c4a478407f6d8
Lucas Sena <lucassenacode@gmail.com>
2025-01-28 14:11:52 -0300

    import.sql ok

--- src/main/java/com/lucassena/springscrudpeople/util/PersonMapper.java
+++ src/main/java/com/lucassena/springscrudpeople/util/PersonMapper.java
@@ -3,10 +3,13 @@ package com.lucassena.springscrudpeople.util;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Component;
+
 import com.lucassena.springscrudpeople.dto.request.PersonRequestDTO;
 import com.lucassena.springscrudpeople.dto.response.PersonResponseDTO;
 import com.lucassena.springscrudpeople.entity.Person;
 
+@Component
 public class PersonMapper {
 
   public Person toPerson(PersonRequestDTO personDTO) {
@@ -37,20 +40,19 @@ public class PersonMapper {
     return peopleList.stream().map(PersonResponseDTO::new).collect(Collectors.toList());
   }
 
-  public void updatePersonData(Person person, PersonRequestDTO personRequestDTO){
+  public void updatePersonData(Person person, PersonRequestDTO personRequestDTO) {
 
     person.setNumQuadrinho(personRequestDTO.getNumQuadrinho());
     person.setNome(personRequestDTO.getNome());
-    person.setApelido(personRequestDTODTO.getApelido());
-    person.setAnoFormatura(personRequestDTODTO.getAnoFormatura());
-    person.setCursocurso(personRequestDTO.getCurso());
-        .cidade(personDTO.getCidade())
-        .estado(personDTO.getEstado())
-        .cep(personDTO.getCep())
-        .telefone(personDTO.getTelefone())
-        .email(personDTO.getEMail())
-        .empresaTrabalha(personDTO.getEmpresaTrabalha())
-        .ultimaAtualizacao(personDTO.getUltimaAtualizacao())
-
+    person.setApelido(personRequestDTO.getApelido());
+    person.setAnoFormatura(personRequestDTO.getAnoFormatura());
+    person.setCurso(personRequestDTO.getCurso());
+    person.setCidade(personRequestDTO.getCidade());
+    person.setEstado(personRequestDTO.getEstado());
+    person.setCep(personRequestDTO.getCep());
+    person.setTelefone(personRequestDTO.getTelefone());
+    person.setEmail(personRequestDTO.getEMail());
+    person.setEmpresaTrabalha(personRequestDTO.getEmpresaTrabalha());
+    person.setUltimaAtualizacao(personRequestDTO.getUltimaAtualizacao());
   }
 }
