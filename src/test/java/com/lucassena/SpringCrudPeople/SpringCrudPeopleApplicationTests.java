94b7e32437cb9da1e4f018bd3e961e70d51877d7
Lucas Sena <lucassenacode@gmail.com>
2025-01-27 21:33:10 -0300

    Inicialização do projeto

---
+++ src/test/java/com/lucassena/SpringCrudPeople/SpringCrudPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassena.SpringCrudPeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
